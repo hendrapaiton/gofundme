a80cee943e29e6b05293910fe42eb988a1a75840
Hendra Dwi Saputra <hendra@usa.com>
2022-11-25 21:14:12 +0700

    Scrapy Start a New Project

---
+++ gofundme/spiders/search.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class SearchSpider(scrapy.Spider):
+    name = 'search'
+    allowed_domains = ['www.gofundme.com']
+    start_urls = ['http://www.gofundme.com/']
+
+    def parse(self, response):
+        pass
