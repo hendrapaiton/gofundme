0a8d7759c01bcc0e6143bea4df3b5cb5189ec252
Hendra Dwi Saputra <hendra@usa.com>
2022-11-25 21:19:08 +0700

    Setting User Agents, Bot Obey, Concurrent Download

--- gofundme/settings.py
+++ gofundme/settings.py
@@ -14,13 +14,13 @@ NEWSPIDER_MODULE = 'gofundme.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'gofundme (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
